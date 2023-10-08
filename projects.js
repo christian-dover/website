524ec8714334778752602aa194eabf76b8afcf81
christian dover <55719234+crazycid44@users.noreply.github.com>
2023-10-07 20:14:32 -0600

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,104 @@
+
+  $(document).ready(function(){
+    $("#create_admin_user_btn").click(function(){
+      $("#create_admin_user").toggle(1000);
+    });
+  });
+
+  $(document).ready(function(){
+    $("#makeing_directory_btn").click(function(){
+      $("#makeing_directory").toggle(1000);
+    });
+  });
+
+  $(document).ready(function(){
+    $("#sftp_connection_btn").click(function(){
+      $("#sftp_connection").toggle(1000);
+    });
+  });
+
+  $(document).ready(function(){
+    $("#area_project_btn").click(function(){
+      $("#area_project").toggle(1000);
+    });
+  });
+
+  $(document).ready(function(){
+    $("#Scholarship_btn").click(function(){
+      $("#Scholarship").toggle(1000);
+    });
+  });
+
+  $(document).ready(function(){
+    $("#deans_list_btn").click(function(){
+      $("#deans_list").toggle(1000);
+    });
+  });
+
+  $(document).ready(function(){
+    $("#conversion_btn").click(function(){
+      $("#Conversion").toggle(1000);
+    });
+  });
+
+  $(document).ready(function(){
+    $("#calendar_btn").click(function(){
+      $("#calendar").toggle(1000);
+    });
+  });
+
+  $(document).ready(function(){
+    $("#month_day_year_btn").click(function(){
+      $("#month_day_year").toggle(1000);
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#Password_btn").click(function(){
+      $("#Password").toggle(1000);
+    });
+  });
+
+  $(document).ready(function(){
+    $("#search_btn").click(function(){
+      $("#search").toggle(1000);
+    });
+  });
+
+  $(document).ready(function(){
+    $("#transfer_btn").click(function(){
+      $("#transfer").toggle(1000);
+    });
+  });
+
+  $(document).ready(function(){
+    $("#LVM_btn").click(function(){
+      $("#LVM").toggle(1000);
+    });
+  });
+
+  $(document).ready(function(){
+    $("#security_btn").click(function(){
+      $("#security").toggle(1000);
+    });
+  });
+
+  $(document).ready(function(){
+    $("#linux_projects_btn").click(function(){
+      $("#linux_projects").toggle(1000);
+    });
+  });
+
+  $(document).ready(function(){
+    $("#C_projects_btn").click(function(){
+      $("#C_projects").toggle(1000);
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#Python_projects_btn").click(function(){
+      $("#Python_projects").toggle(1000);
+    });
+  });
+
+  
